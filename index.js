47c37fd89cffcc1a11d0064b8896db57960a9bd2
Hawa <hawamaria33@gmail.com>
2023-06-11 11:32:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+  const element = document.querySelector("#button");
+
+  element.addEventListener("click", function () {
+    alert("I was clicked!");
+  });
 }
+
+addingEventListener();
